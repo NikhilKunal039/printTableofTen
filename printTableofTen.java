9a512182d583a74c315cf69f398483c170c6ec39
NikhilKunal039 <kunalnikhildac@gmail.com>
2024-07-31 13:28:48 +0530

    Add files via upload

---
+++ printTableofTen.java
@@ -0,0 +1,10 @@
+class printTableofTen{
+    public static void main(String[] args) {
+        int number = 10;
+        for(int i=1;i<=10;i++){
+            System.out.println(number+"*"+i+"="+number*i);
+        }
+    }
+}
+
+// git remote -v
